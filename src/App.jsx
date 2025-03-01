04e1cf381d6a4d5ab455821be69e520dc4726102
AjayGaneshram <95750604+AjayGaneshram@users.noreply.github.com>
2025-03-01 08:34:31 +0530

    Updated the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import LetterHomePage from "./components/firstLetter/LetterHomePage";
 import Header from "./components/header/Header";
+import GoogleAnalytics from "./GoogleAnalytics";
 
 function App() {
+  GoogleAnalytics("G-ZB0KBY3QVK");
   return (
     <div>
       <LetterHomePage/>
